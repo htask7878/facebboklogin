5d69d8927f996ac7389a472ac98342d36ebbfd97
htask7878 <pdfile005@gmail.com>
2022-12-13 12:03:37 +0530

    Initial commit

--- lib/home.dart
+++ lib/home.dart
@@ -1,8 +1,22 @@
 import 'package:facebboklogin/Model.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
 import 'package:provider/provider.dart';
 
 class home extends StatelessWidget {
+  Future<UserCredential> signInWithFacebook() async {
+    // Trigger the sign-in flow
+    final LoginResult loginResult = await FacebookAuth.instance.login();
+
+    // Create a credential from the access token
+    final OAuthCredential facebookAuthCredential =
+        FacebookAuthProvider.credential(loginResult.accessToken!.token);
+
+    // Once signed in, return the UserCredential
+    return FirebaseAuth.instance.signInWithCredential(facebookAuthCredential);
+  }
+
   @override
   Widget build(BuildContext context) {
     Model model = Provider.of(context);
@@ -12,9 +26,14 @@ class home extends StatelessWidget {
       body: SafeArea(
         child: Column(
           children: [
-            TextField(controller: model.t1,keyboardType: TextInputType.emailAddress),
-            const SizedBox(height: 10,),
+            TextField(
+                controller: model.t1, keyboardType: TextInputType.emailAddress),
+            const SizedBox(
+              height: 10,
+            ),
+            ElevatedButton(onPressed: () {
 
+            }, child: Text("Login"))
           ],
         ),
       ),
